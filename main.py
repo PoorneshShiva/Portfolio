01f882492148b760406e628a427f858b561adae8
PoorneshShiva <poorneshshiva@gmail.com>
2022-03-04 13:48:09 +0530

    Intial_Commit

---
+++ main.py
@@ -0,0 +1,60 @@
+from flask import Flask, render_template, request, url_for, flash, get_flashed_messages
+from flask_bootstrap import Bootstrap
+from morse import encryption_to_morse, decryption_to_english
+
+app = Flask(__name__)
+Bootstrap(app)
+
+
+@app.route('/', methods=["POST", "GET"])
+def home():
+    return render_template('index.html')
+
+
+@app.route('/photography', methods=["POST", "GET"])
+def photography():
+    return render_template('photography.html')
+
+
+@app.route('/videography', methods=["POST", "GET"])
+def videography():
+    return render_template('videography.html')
+
+
+@app.route('/music', methods=["POST", "GET"])
+def music():
+    return render_template('music.html')
+
+
+@app.route('/project', methods=["POST", "GET"])
+def project():
+    return render_template('project.html')
+
+
+@app.route('/decryption', methods=["POST", "GET"])
+def decryption():
+    if request.method == "POST":
+        decrypted_data = ""
+        data = request.form["decrypted_data"]
+        print(data)
+        decrypted_data = decryption_to_english(data)
+        return render_template('project.html', decryption=True, data=decrypted_data)
+
+
+@app.route('/encryption', methods=["POST", "GET"])
+def encryption():
+    if request.method == "GET":
+
+        pass
+    else:
+        encrypted_data = ""
+        data = request.form["encrypted_data"]
+        print(data)
+        encrypted_data = encryption_to_morse(data)
+        print(encrypted_data)
+
+        return render_template('project.html', decryption=False, data=encrypted_data)
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0")
---
+++ morse.py
@@ -0,0 +1,68 @@
+from logo import banner
+import pyperclip
+
+ENGLISH_TO_MORSE = {'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..', 'E': '.', 'F': '..-.', 'G': '--.', 'H': '....',
+                    'I': '..', 'J': '.---', 'K': '-.-', 'L': '.-..', 'M': '--', 'N': '-.', 'O': '---', 'P': '.--.',
+                    'Q': '--.-', 'R': '.-.', 'S': '...', 'T': '-', 'U': '..-', 'V': '...-', 'W': '.--', 'X': '-..-',
+                    'Y': '-.--', 'Z': '--..', '0': '-----', '1': '.----', '2': '..---', '3': '...--', '4': '....-',
+                    '5': '.....', '6': '-....', '7': '--...', '8': '---..', '9': '----.', ' ': ' '}
+DECRYPTED_MESSAGE = ""
+ENCRYPTION_MESSAGE = ""
+
+
+def encryption_to_morse(word):
+    """Encrypts the English Words to Morse Code, Required few English Words."""
+    global ENCRYPTION_MESSAGE
+    ENCRYPTION_MESSAGE = ""
+    for letter in word.upper():
+        new_letter = ENGLISH_TO_MORSE[letter]
+        ENCRYPTION_MESSAGE += new_letter + "/"
+    pyperclip.copy(ENCRYPTION_MESSAGE)
+    return ENCRYPTION_MESSAGE
+
+
+def decryption_to_english(word):
+    """Decrypts the Morse Code to English Words, Required few Morse code."""
+    global DECRYPTED_MESSAGE
+    DECRYPTED_MESSAGE = ""
+    for each in word.split("/"):
+        values = list(ENGLISH_TO_MORSE.values())
+        keys = list(ENGLISH_TO_MORSE.keys())
+        if each in values:
+            decrypt_letter = keys[values.index(each)]
+            DECRYPTED_MESSAGE += decrypt_letter
+        pyperclip.copy(DECRYPTED_MESSAGE)
+    return DECRYPTED_MESSAGE.title()
+
+
+# Printing the Banner!
+print(banner)
+
+
+def main():
+    #
+    # Asking the User to enter the requirements.
+    choice = input('Do you want to encode(e), decode(d), or quit(q)?:').lower()
+    if choice == "e":
+        encode = input('Enter the English code to decode?:').upper()
+        encoded_text = encryption_to_morse(encode)
+        # Copies the encoded text.
+        pyperclip.copy(encoded_text)
+        print("Your encoded text is copied to your clipboard Successfully!")
+        print(f"Your encoded text is {encoded_text}")
+
+    elif choice == "d":
+        decode = input('Enter the Morse code to decode?:').upper()
+        decoded_text = decryption_to_english(decode)
+        # Copies the decoded text.
+        pyperclip.copy(decoded_text)
+        print("Your decoded text is copied to your clipboard Successfully!")
+        print(f"Your decoded text is '{decoded_text}'")
+    elif choice == "q":
+        end = True
+    else:
+        print("Sorry you've chosen a wrong choice!")
+
+
+if __name__ == "__main__":
+    main()
