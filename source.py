e09efc101842763708add49be7b8917537c1c995
PoorneshShiva <poorneshshiva@gmail.com>
2022-10-01 17:58:55 +0530

    Update with source file

---
+++ source.py
@@ -0,0 +1,20 @@
+homepage_gallery = [{"link": "static/images/Photography_of_mine.jpg", "Heading": "Capturing Photo",
+                     "Paragraph": "Esha in Coimbatore,India"},
+                    {"link": "static/images/victor-rutka-1zfqWQdSHUc-unsplash.jpg", "Heading": "Capturing Video",
+                     "Paragraph": "I will Update Mine Soon!"},
+                    {"link": "static/images/lijo-joseph-saSAJ3zoTCI-unsplash.jpg", "Heading": "Hands on the Piano!",
+                     "Paragraph": "I will Update Mine Soon!"},
+                    {"link": "static/images/computer.jpg", "Heading": "Hands on my Computer!",
+                     "Paragraph": "I will Update Mine Soon!"}]
+
+books_knowledge = [{"link": "static/images/napoleon-hill.jpg", "Heading": "Napoleon Hill",
+                     "Paragraph": "Tell the World what you intend to do, but first show it.", "active": True},
+                    {"link": "static/images/sadhguru1.width-1024.jpg", "Heading": "Sadguru",
+                     "Paragraph": "Karma is not some external system of crime and punishment"},
+                    {"link": "static/images/Robin-Sharma-Quotes-.jpg", "Heading": " Robin Sharma ",
+                     "Paragraph": "The duty of every Human-Being is not self-improvement but self remembering"}]
+
+print(books_knowledge[0]["link"])
+
+print(("0.00000000000000000000000000000000000000000").count("0"))
+
