56279b1daeeb717a240dce4a70b7fb1bf3992372
PoorneshShiva <poorneshshiva@gmail.com>
2022-10-11 16:15:19 +0530

    Update with Photography gallery.

--- source.py
+++ source.py
@@ -14,7 +14,13 @@ books_knowledge = [{"link": "static/images/napoleon-hill.jpg", "Heading": "Napol
                     {"link": "static/images/Robin-Sharma-Quotes-.jpg", "Heading": " Robin Sharma ",
                      "Paragraph": "The duty of every Human-Being is not self-improvement but self remembering"}]
 
-print(books_knowledge[0]["link"])
+photography_gallary = [
+{'caption': 'Mannavanur, Tamilnadu', 'link': 'https://images.unsplash.com/photo-1665481193027-d5d2c5ee9fc3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwcm9maWxlLXBhZ2V8OHx8fGVufDB8fHx8&auto=format&fit=crop&w=1632&q=80'},
+    {'caption': 'Hill station Koddaikannal, Tamilnadu', 'link': 'https://images.unsplash.com/photo-1665481193520-86e5eaba1af6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwcm9maWxlLXBhZ2V8MXx8fGVufDB8fHx8&auto=format&fit=crop&w=1632&q=80'},
+    {'caption': 'Isha Coimbatore,Tamilnadu', 'link': 'https://images.unsplash.com/photo-1665481485578-4e54cf97c917?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwcm9maWxlLXBhZ2V8NXx8fGVufDB8fHx8&auto=format&fit=crop&w=1632&q=80'},
+    {'caption': 'Hill station Koddaikannal, Tamilnadu', 'link': 'https://images.unsplash.com/photo-1665481194192-f7067c3b8a80?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwcm9maWxlLXBhZ2V8NHx8fGVufDB8fHx8&auto=format&fit=crop&w=1632&q=80'},
+    {'caption': 'Dindugul-Kodaikannal,TamilNadu', 'link': 'https://images.unsplash.com/photo-1665481327746-8f05f77a88a2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwcm9maWxlLXBhZ2V8M3x8fGVufDB8fHx8&auto=format&fit=crop&w=1632&q=80'},
+    {'caption': 'Isha Coimbatore,Tamilnadu', 'link': 'https://images.unsplash.com/photo-1665481485518-c693644d6f91?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwcm9maWxlLXBhZ2V8Nnx8fGVufDB8fHx8&auto=format&fit=crop&w=1632&q=80'},
+    {'caption': 'Resort Kodaikannal, TamilNadu', 'link': 'https://images.unsplash.com/photo-1665481312388-2de69b25cec8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwcm9maWxlLXBhZ2V8N3x8fGVufDB8fHx8&auto=format&fit=crop&w=1632&q=80'}]
 
-print(("0.00000000000000000000000000000000000000000").count("0"))
-
+# "https://images.unsplash.com/photo-1665481193520-86e5eaba1af6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1632&q=80"
